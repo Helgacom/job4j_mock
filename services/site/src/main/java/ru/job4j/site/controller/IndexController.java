3232523d61959b10aa7b50ee8f67a41680f7858d
Olga Remneva <remneva_olga@mail.ru>
2024-10-10 14:26:55 +0400

    RestTemplate

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,12 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.site.dto.InterviewDTO;
 import ru.job4j.site.dto.ProfileDTO;
 import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -26,6 +29,7 @@ public class IndexController {
     private final AuthService authService;
     private final NotificationService notifications;
     private final ProfilesService profilesService;
+    private final TopicsService topicsService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
@@ -50,6 +54,13 @@ public class IndexController {
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.toList());
+        Map<Integer, Integer> sizeCategoryInterview = new HashMap<>();
+        for (InterviewDTO interview : newInterviews) {
+            int categoryId = topicsService.getById(interview.getTopicId()).getCategory().getId();
+            Integer count = sizeCategoryInterview.get(categoryId);
+            sizeCategoryInterview.put(categoryId, count == null ? 1 : ++count);
+        }
+        model.addAttribute("sizeCategory", sizeCategoryInterview);
         model.addAttribute("new_interviews", newInterviews);
         model.addAttribute("users", userList);
         return "index";
