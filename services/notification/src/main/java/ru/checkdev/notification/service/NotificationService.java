473c3584902ad3ad61f40312ee621ad0b759ba21
Olga Remneva <remneva_olga@mail.ru>
2024-10-22 12:48:49 +0400

    SpringBoot + Kafka

--- services/notification/src/main/java/ru/checkdev/notification/service/NotificationService.java
+++ services/notification/src/main/java/ru/checkdev/notification/service/NotificationService.java
@@ -1,30 +1,21 @@
 package ru.checkdev.notification.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.checkdev.notification.domain.Notify;
-
-import javax.annotation.PreDestroy;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 @Service
 public class NotificationService {
 
-    private final TemplateService templates;
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final KafkaTemplate<String, Notify> kafkaTemplate;
 
     @Autowired
-    public NotificationService(final TemplateService templates) {
-        this.templates = templates;
+    public NotificationService(final KafkaTemplate<String, Notify> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     public void put(final Notify notify) {
-        this.scheduler.execute(() -> this.templates.send(notify));
-    }
-
-    @PreDestroy
-    public void close() {
-        this.scheduler.shutdown();
+        kafkaTemplate.send("notify", notify);
     }
 }
